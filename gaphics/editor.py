ad21d82e20c69a022212c6d8488747b0c3bd2008
janerik <janerikvoigt@gmail.com>
2021-08-04 23:04:42 +0200

    implemented Surface as a base

--- gaphics/editor.py
+++ gaphics/editor.py
@@ -1,6 +1,6 @@
 import pygame
 import gui.button as boo
-
+import fonts.font_manager as font_manager
 
 pygame.init()
 screen = pygame.display.set_mode((700,400))
@@ -11,6 +11,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    button = boo.Button((100,100))
+    button = boo.Button((100,100),"hello World!")
     screen.blit(button.draw(),(0,0))
     pygame.display.flip()
--- gaphics/gui/button.py
+++ gaphics/gui/button.py
@@ -4,11 +4,16 @@ from .surface import Surface as custSurface
 
 class Button(custSurface):
 
-    def __init__(self, rect):
-        self.rect = rect
-        self.bg_col = pygame.Color(200,200,200)
+    def __init__(self, rect, text):
+        super().__init__(rect)
+
+        self.text = text
+
 
     def draw(self):
-        img = pygame.Surface(self.rect)
-        img.fill(self.bg_col)
+        img = super().draw()
+
+        #text
+
+
         return img
